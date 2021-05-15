ccee4b075a3a93b298e2bdd870abd31051ee9bcc
halanda <55902431+halanda@users.noreply.github.com>
2021-05-15 15:01:26 +0800

    添加admincontroller

---
+++ src/main/java/org/demo/cn/controller/AdminController.java
@@ -0,0 +1,32 @@
+package org.demo.cn.controller;
+
+import org.demo.cn.pojo.AffectedNum;
+import org.demo.cn.serivce.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/admin")
+@Api(tags = "AdminController Interface")
+public class AdminController {
+	@Autowired
+	private CityService cityService;
+
+	@ApiOperation("City date batch insert by json file")
+	@GetMapping("/addcities")
+	public ResponseEntity<AffectedNum> insertCityBatch() {
+		
+		int num = cityService.insertCityBatch();
+		
+		AffectedNum an = new AffectedNum(num, "Cities added successfully");
+		
+		return new ResponseEntity<AffectedNum>(an, HttpStatus.OK);
+	}
+}
