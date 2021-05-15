d96ad511126f851413023791f847e9c563bcf80e
halanda <55902431+halanda@users.noreply.github.com>
2021-05-15 15:05:57 +0800

    批量插入城市数据 遗落版

--- src/main/java/org/demo/cn/serivce/CityService.java
+++ src/main/java/org/demo/cn/serivce/CityService.java
@@ -5,4 +5,6 @@ import org.demo.cn.pojo.CityPojo;
 public interface CityService {
 	
 	Integer insertCity(CityPojo cityPojo);
+	
+	Integer insertCityBatch();
 }
--- src/main/java/org/demo/cn/serivce/CityServiceImpl.java
+++ src/main/java/org/demo/cn/serivce/CityServiceImpl.java
@@ -2,24 +2,63 @@ package org.demo.cn.serivce;
 
 import org.demo.cn.mapper.CityMapper;
 import org.demo.cn.pojo.CityPojo;
+import org.demo.cn.util.JSONObjectConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Iterator;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 @Service
 public class CityServiceImpl implements CityService {
 
+	 private Logger logger = LoggerFactory.getLogger(CityServiceImpl.class);
+	
 	@Autowired
 	private CityMapper cityMapper;
 	
 	@Override
-	@Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRED)
+	@Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.NESTED)
 	public Integer insertCity(CityPojo cityPojo) {
 		
 		Integer num = cityMapper.insertCity(cityPojo);
 		
 		return num;
+	}
+	/**
+	 * 
+	 * return -- the #rows inserted
+	 */
+	@Override
+	@Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRES_NEW)
+	public Integer insertCityBatch() {
+		
+		String filePath = "citylist.json";
+		TypeReference<List<CityPojo>> typeReference = new TypeReference<List<CityPojo>>() {};
+		List<CityPojo> cityList = JSONObjectConverter.generateObjectFromJSONFile(filePath, typeReference);
+		Iterator<CityPojo> it = cityList.iterator();
+		int total = cityList.size();
+		int duplicated = 0;
+		CityPojo city = null;
+		while(it.hasNext()) {
+			city = it.next();
+			try {
+				this.insertCity(city);
+			} catch (Exception e) {
+				if(city != null) {
+					logger.error("Duplicated record: " + city.getName() + " -> " + city.getId());
+				}else {
+					logger.error("Unable to get City Infor from JSON");
+				}
+				duplicated++;
+			}
+		} 
+		
+		return total - duplicated;
 	}
 }
